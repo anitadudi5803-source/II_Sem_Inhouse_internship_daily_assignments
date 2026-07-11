3933aa7370ea28979e90ab62872913a0524f0bf6
anitadudi5803-source <anitadudi5803@gmail.com>
2026-07-11 23:00:06 +0530

    Added day4 to day10 assignments

---
+++ day10/dashboard.php
@@ -0,0 +1,20 @@
+<?php
+include ("dashboardheader.php");
+include("dashboardVertical.php");
+?>
+// session_start();
+
+// if(!isset($_SESSION['user_name'])){
+//     header("Location: login.php");
+//     exit();
+// }
+
+       <h2>
+         <?php echo "Welcome, ". $_SESSION['user_name']. "!";
+  ?>
+</h2>
+
+<?php
+include("dashboardFooter.php");
+include("footer.php");
+?>
